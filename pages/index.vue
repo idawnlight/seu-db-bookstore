07603fa998e673fef48e829658b2e5f0b8b2af70
idawnlight <idawn@live.com>
2023-12-27 20:19:38 +0800

    feat: search

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,40 @@
 <script setup lang="ts">
-const cart = useCartStore()
+import { Search } from '@vicons/ionicons5'
 
-const books = (await useFetch('/api/book/list')).data
+const cart = useCartStore()
+const searchKey = ref('')
+
+const booksReq = await useFetch('/api/book/list', {
+    query: {
+        keyword: searchKey
+    }
+})
+const books = computed(() => {
+    const data = booksReq.data?.value;
+    if (data && 'data' in data) {
+        return data.data;
+    }
+})
+
+let searchTimer = setTimeout(() => {}, 0);
+const onSearch = (value: string) => {
+    clearTimeout(searchTimer);
+    searchTimer = setTimeout(() => {
+        searchKey.value = value;
+    }, 200)
+}
 </script>
 
 <template>
+    <n-input placeholder="Search" @input="onSearch" class="mb-4">
+        <template #prefix>
+            <n-icon>
+                <Search />
+            </n-icon>
+        </template>
+    </n-input>
     <div class="grid gap-2 grid-cols-6">
-        <BookItem v-for="book in books?.data" :book="book" @add-to-cart="cart.add(book.id)"></BookItem>
+        <BookItem v-for="book in books" :book="book" @add-to-cart="cart.add(book.id)"></BookItem>
     </div>
-
-    <NuxtLink to="/hello">
-        <n-button>Go to Hello page</n-button>
-    </NuxtLink>
+    <div v-if="books && books.length === 0" class="text-xl">No books found.</div>
 </template>
--- server/api/book/list.ts
+++ server/api/book/list.ts
@@ -1,9 +1,43 @@
 import prisma from "~/server/helpers/prisma";
 
 export default defineEventHandler(async (event) => {
-    const books = await prisma.book.findMany();
-    return {
-        status: 200,
-        data: books
+    const query = getQuery(event);
+    const keyword = query.keyword as string;
+
+    console.log('keyword', keyword);
+
+    if (!keyword) {
+        const books = await prisma.book.findMany();
+        return {
+            status: 200,
+            data: books
+        }
+    } else {
+        const books = await prisma.book.findMany({
+            where: {
+                OR: [
+                    {
+                        title: {
+                            contains: keyword
+                        }
+                    },
+                    {
+                        author: {
+                            contains: keyword
+                        }
+                    },
+                    {
+                        description: {
+                            contains: keyword
+                        }
+                    }
+                ]
+            }
+        });
+
+        return {
+            status: 200,
+            data: books
+        }
     }
 });
